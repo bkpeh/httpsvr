33094cd780c97de50c419c9d6391df9b1113c83a
bkpeh <bkpeh10@yahoo.com>
2021-01-18 00:34:23 +0800

    Update 180121:1234

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/bkpeh/httpsvr
 go 1.15
 
 //replace github.com/bkpeh/httpsvr/util => /Users/owner/Go/src/github.com/bkpeh/httpsvr/util
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v0.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v0.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	logging "github.com/bkpeh/httpsvr/util"
 	hsvr "github.com/bkpeh/httpsvr/web"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 func main() {
@@ -13,7 +20,56 @@ func main() {
 
 	//defer logging.GetLogFile().Close()
 
-	err := http.ListenAndServe(":8080", nil)
+	//cfg, err := external.LoadDefaultAWSConfig()
+
+	//config, err := config.LoadDefaultConfig(context.TODO())
+
+	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		if service == dynamodb.ServiceID && region == "us-west-2" {
+			return aws.Endpoint{
+				PartitionID:   "aws",
+				URL:           "http://localhost:8000",
+				SigningRegion: "us-west-2",
+			}, nil
+		}
+		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+
+	cfg, _ := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"), config.WithEndpointResolver(customResolver))
+
+	svc := dynamodb.NewFromConfig(cfg)
+
+	// Build the request with its input parameters
+	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5)})
+
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	fmt.Println("Tables:")
+	for _, tableName := range resp.TableNames {
+		fmt.Println(tableName)
+	}
+	/*
+	   	// Set the AWS Region that the service clients should use
+	   	//cfg.Region = endpoints.UsWest2RegionID
+	   	sess := session.Must(session.NewSessionWithOptions(session.Options{
+	   		Config: aws.Config{
+	   			Region:           "us-west-2",
+	   			EndpointResolver: endpoints.ResolverFunc(customResolver),
+	   		},
+	   	}))
+	   	// Using the Config value, create the DynamoDB client
+	   	svc := dynamodb.New(sess)
+
+	   	// Build the request with its input parameters
+	   	req := svc.DescribeTableRequest(&dynamodb.DescribeTableInput{
+	       TableName: aws.String("myTable"),
+	   })
+	*/
+	err = http.ListenAndServe(":8080", nil)
 
 	if err != nil {
 		logging.LogError("Main:" + err.Error())
