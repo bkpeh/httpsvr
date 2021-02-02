eef135306c1afb035179aa70b3390b4fa1a5007d
bkpeh <bkpeh10@yahoo.com>
2021-02-02 11:33:57 +0800

    Update 020221:1133

--- go.mod
+++ go.mod
@@ -2,9 +2,11 @@ module github.com/bkpeh/httpsvr
 
 go 1.15
 
-//replace github.com/bkpeh/httpsvr/util => /Users/owner/Go/src/github.com/bkpeh/httpsvr/util
+//replace github.com/bkpeh/httpsvr/util => /Users/user/Go/src/github.com/bkpeh/httpsvr/util
 
 require (
-	github.com/aws/aws-sdk-go-v2/config v0.4.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/dynamodb v0.31.0 // indirect
+	github.com/aws/aws-sdk-go v1.36.28
+	github.com/aws/aws-sdk-go-v2 v0.31.0
+	github.com/aws/aws-sdk-go-v2/config v0.4.0
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v0.31.0
 )
--- main.go
+++ main.go
@@ -1,98 +1,58 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"net/http"
 
 	logging "github.com/bkpeh/httpsvr/util"
 	hsvr "github.com/bkpeh/httpsvr/web"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 func main() {
 	hsvr.SetLog("logs/http.log")
 	http.HandleFunc("/", hsvr.Index)
 
-	//defer logging.GetLogFile().Close()
-
-	//cfg, err := external.LoadDefaultAWSConfig()
-
-	//config, err := config.LoadDefaultConfig(context.TODO())
-
-	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-		if service == dynamodb.ServiceID && region == "us-west-2" {
-			return aws.Endpoint{
-				PartitionID:   "aws",
-				URL:           "http://localhost:8000",
-				SigningRegion: "us-west-2",
-			}, nil
-		}
-		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
-		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
-	})
-
-	cfg, _ := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"), config.WithEndpointResolver(customResolver))
-
-	svc := dynamodb.NewFromConfig(cfg)
-
-	output, err := svc.Query(context.TODO(), &dynamodb.QueryInput{
-		TableName:                 new(string),
-		AttributesToGet:           []string{},
-		ConditionalOperator:       "",
-		ConsistentRead:            new(bool),
-		ExclusiveStartKey:         map[string]types.AttributeValue{},
-		ExpressionAttributeNames:  map[string]string{},
-		ExpressionAttributeValues: map[string]types.AttributeValue{":i": &types.AttributeValueMemberS{"1020"}},
-		FilterExpression:          new(string),
-		IndexName:                 new(string),
-		KeyConditionExpression:    aws.String("EID = :i"),
-		KeyConditions:             map[string]types.Condition{},
-		Limit:                     new(int32),
-		ProjectionExpression:      new(string),
-		QueryFilter:               map[string]types.Condition{},
-		ReturnConsumedCapacity:    "",
-		ScanIndexForward:          new(bool),
-		Select:                    "",
-	})
-	// Build the request with its input parameters
-	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
-		Limit: aws.Int32(5)})
-
-	fmt.Println("output:", output)
-	if err != nil {
-		log.Fatalf("failed to list tables, %v", err)
-	}
-
-	fmt.Println("Tables:")
-	for _, tableName := range resp.TableNames {
-		fmt.Println(tableName)
-	}
-	/*
-	   	// Set the AWS Region that the service clients should use
-	   	//cfg.Region = endpoints.UsWest2RegionID
-	   	sess := session.Must(session.NewSessionWithOptions(session.Options{
-	   		Config: aws.Config{
-	   			Region:           "us-west-2",
-	   			EndpointResolver: endpoints.ResolverFunc(customResolver),
-	   		},
-	   	}))
-	   	// Using the Config value, create the DynamoDB client
-	   	svc := dynamodb.New(sess)
-
-	   	// Build the request with its input parameters
-	   	req := svc.DescribeTableRequest(&dynamodb.DescribeTableInput{
-	       TableName: aws.String("myTable"),
-	   })
-	*/
-	err = http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
 		logging.LogError("logs/http.log", "Main:"+err.Error())
 	}
+	/*
+		customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+			if service == dynamodb.ServiceID && region == "us-west-2" {
+				return aws.Endpoint{
+					PartitionID:   "aws",
+					URL:           "http://localhost:8000",
+					SigningRegion: "us-west-2",
+				}, nil
+			}
+			// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+			return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+		})
+
+		cfg, _ := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"), config.WithEndpointResolver(customResolver))
+
+		svc := dynamodb.NewFromConfig(cfg)
+
+		a := types.AttributeValueMemberS{Value: `"S":"easter@gmail.com"`}
+		//a := types.AttributeValueMemberS{S: "easter@gmail.com"}
+		output, err := svc.GetItem(context.TODO(), &dynamodb.GetItemInput{
+			TableName: aws.String("People"),
+			Key:       map[string]types.AttributeValue{"Email": &a},
+		})
+
+		fmt.Println("output:", output)
+
+		// Build the request with its input parameters
+		resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+			Limit: aws.Int32(5)})
+
+		if err != nil {
+			log.Fatalf("failed to list tables, %v", err)
+		}
+
+		fmt.Println("Tables:")
+		for _, tableName := range resp.TableNames {
+			fmt.Println(tableName)
+		}
+	*/
 }
